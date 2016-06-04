6c50e1497e2d77a79e2c7800fc5825a4b530b34f
Eric Fay <edf4550@gmail.com>
2016-06-04 17:39:04 -0400

    major release

--- scripts/action.js
+++ scripts/action.js
@@ -1,7 +1,34 @@
 $(document).on("submit", "#queryForm", function(event) {
   var $form = $(this);
   $.post($form.attr("action"), $form.serialize(), function(responseJson) {
-      $("#result").text(responseJson.type);
+
+    L.geoJson(responseJson, {
+      onEachFeature: function(feature, layer) {
+        var popupContent = "<p>The date " +
+          feature.properties.timestamp + " is estimated to be located at " +
+          feature.geometry.coordinates + ".</p>";
+          
+        if (feature.properties && feature.properties.popupContent) {
+          popupContent += feature.properties.popupContent;
+        }
+        layer.bindPopup(popupContent);
+      }
+    }).addTo(map);
+
+    var items = [];
+    $.each( responseJson.features, function( index, feature ) {
+      items.push("<tr id='feature'>");
+      items.push( "<td id='coordinates'>" 
+        + feature.geometry.coordinates + "</td>" );
+      items.push( "<td id='timestamp'>" 
+        + feature.properties.timestamp + "</td>" );
+      items.push("</tr>");
+    });
+   
+    $( "<table/>", {
+      "class": "table table-striped",
+      html: items.join( "" )
+    }).appendTo( "#results" );
   });
   event.preventDefault(); 
 });
