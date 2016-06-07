ebd0591c45c16c791b3e5151fcd3c4ef685d6e70
Eric Fay <edf4550@gmail.com>
2016-06-07 15:59:49 -0400

    improved ui

--- scripts/parse.js
+++ scripts/parse.js
@@ -1,5 +1,5 @@
 function print(str) {
-  $("#parsed").text(str);
+  $("#parsed").append(str);
 }
 function printJSON(json) {
   print(JSON.stringify(json));
@@ -20,9 +20,45 @@ function mapPoint(point) {
   }).addTo(map);
 }
 
+function truncate(number) {
+  return number > 0 
+    ? Math.floor(number)
+    : Math.ceil(number);
+}
+// geojson coordinate [longitude,latitude]
+// common order is latitude then longitude
+function coordinateFormat(point) {
+  var strArray = [];
+  var deg, min, sec;
+  var compassDir;
+  $.each( point.coordinates, function(index, coordinate) {
+    switch(index) {
+      case 0: compassDir = coordinate > 0 ? "W" : "E"; break;
+      case 1: compassDir = coordinate > 0 ? "N" : "S"; break;
+    }
+    coordinate = Math.abs(coordinate);
+    deg = truncate(coordinate);
+    min = truncate(
+      (coordinate - deg) * 60
+    );
+    sec = Math.abs(
+      (((coordinate - deg) * 60) - min) * 60
+    ).toFixed(2);
+    strArray.push( 
+        deg.toString() + "\u00B0"
+      + min.toString() + "\'"
+      + sec.toString() + "\""
+      + compassDir
+    );
+  });
+  return strArray[1] + " and " + strArray[0];
+}
+
 function parse(str) {
   $.get("gps-parse", str, function(geojson){
-    printJSON(geojson);
+//    printJSON(geojson);
+    print(coordinateFormat(geojson.geometry));
+    print("\n");
     mapPoint(geojson.geometry)
   }, "json");
 }
